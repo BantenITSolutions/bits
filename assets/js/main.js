25d2b6fc2c7bf3cbf6a21b322e68bd200cce0ee3
Nurul Imam <me@nurulimam.com>
2015-01-21 19:19:29 +0700

    Release

---
+++ assets/js/main.js
@@ -0,0 +1,139 @@
+jQuery(function($) {'use strict',
+
+	//#main-slider
+	$(function(){
+		$('#main-slider.carousel').carousel({
+			interval: 8000
+		});
+	});
+
+
+	// accordian
+	$('.accordion-toggle').on('click', function(){
+		$(this).closest('.panel-group').children().each(function(){
+		$(this).find('>.panel-heading').removeClass('active');
+		 });
+
+	 	$(this).closest('.panel-heading').toggleClass('active');
+	});
+
+	//Initiat WOW JS
+	new WOW().init();
+
+	// portfolio filter
+	$(window).load(function(){'use strict';
+		var $portfolio_selectors = $('.portfolio-filter >li>a');
+		var $portfolio = $('.portfolio-items');
+		$portfolio.isotope({
+			itemSelector : '.portfolio-item',
+			layoutMode : 'fitRows'
+		});
+
+		$portfolio_selectors.on('click', function(){
+			$portfolio_selectors.removeClass('active');
+			$(this).addClass('active');
+			var selector = $(this).attr('data-filter');
+			$portfolio.isotope({ filter: selector });
+			return false;
+		});
+	});
+
+	// Contact form
+	var form = $('#main-contact-form');
+	form.submit(function(event){
+		event.preventDefault();
+		var form_status = $('<div class="form_status"></div>');
+		$.ajax({
+			url: $(this).attr('action'),
+
+			beforeSend: function(){
+				form.prepend( form_status.html('<p><i class="fa fa-spinner fa-spin"></i> Email is sending...</p>').fadeIn() );
+			}
+		}).done(function(data){
+			form_status.html('<p class="text-success">' + data.message + '</p>').delay(3000).fadeOut();
+		});
+	});
+
+
+	//goto top
+	$('.gototop').click(function(event) {
+		event.preventDefault();
+		$('html, body').animate({
+			scrollTop: $("body").offset().top
+		}, 500);
+	});
+
+	//Pretty Photo
+	$("a[rel^='prettyPhoto']").prettyPhoto({
+		social_tools: false
+	});
+});
+
+
+var wow = new WOW(
+  {
+    boxClass:     'wowload',      // animated element css class (default is wow)
+    animateClass: 'animated', // animation css class (default is animated)
+    offset:       0,          // distance to the element when triggering the animation (default is 0)
+    mobile:       true,       // trigger animations on mobile devices (default is true)
+    live:         true        // act on asynchronously loaded content (default is true)
+  }
+);
+wow.init();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+
+
+
+  $("div.blog-post").hover(
+    function() {
+        $(this).find("div.content-hide").slideToggle("fast");
+    },
+    function() {
+        $(this).find("div.content-hide").slideToggle("fast");
+    }
+  );
+
+  $('.flexslider').flexslider({
+        prevText: '',
+        nextText: ''
+    });
+
+  $('.testimonails-slider').flexslider({
+    animation: 'slide',
+    slideshowSpeed: 5000,
+    prevText: '',
+    nextText: '',
+    controlNav: false
+  });
+
+  $(function(){
+
+  // Instantiate MixItUp:
+
+  $('#Container').mixItUp();
+
+
+
+  $(document).ready(function() {
+      $(".fancybox").fancybox();
+    });
+
+  });
+
+
+});
+
