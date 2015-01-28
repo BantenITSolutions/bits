87eaa14bb095f7a7562906ebb5301bd21d2b95ed
Nurul Imam <me@nurulimam.com>
2015-01-28 18:00:37 +0700

    Karya

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,3 +43,28 @@ $(document).ready(function() {
 
 });
 
+jQuery(function($) {
+    //portfolio
+    $(window).load(function(){
+        $portfolio_selectors = $('.portfolio-filter >li>a');
+        if($portfolio_selectors!='undefined'){
+            $portfolio = $('.portfolio-items');
+            $portfolio.isotope({
+                itemSelector : 'li',
+                layoutMode : 'fitRows'
+            });
+            $portfolio_selectors.on('click', function(){
+                $portfolio_selectors.removeClass('active');
+                $(this).addClass('active');
+                var selector = $(this).attr('data-filter');
+                $portfolio.isotope({ filter: selector });
+                return false;
+            });
+        }
+    });
+
+    //Pretty Photo
+    $("a[rel^='prettyPhoto']").prettyPhoto({
+        social_tools: false
+    });
+});
